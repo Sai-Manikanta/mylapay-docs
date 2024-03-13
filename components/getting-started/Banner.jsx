c5bee3e00e59a5ebaadd9bd6f3225b895bd7d2c3
Sai Manikanta <mani333007@gmail.com>
2024-03-13 15:13:57 +0530

    Products button link

--- components/getting-started/Banner.jsx
+++ components/getting-started/Banner.jsx
@@ -12,7 +12,7 @@ function Banner() {
                 </p>
 
                 <div className="m-auto w-full text-center lg:text-left">
-                    <a href="/contact">
+                    <a href="/product-apis">
                         <button className="mx-3 font-semibold px-5 w-52 text-white text-center sm:mr-2 sm:ml-0 text-sm rounded-md bg-bluedark hover:before:bg-bluelight relative h-[50px] overflow-hidden px-3 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:transition-all before:duration-500 hover:text-white hover:before:left-0 hover:before:w-full uppercase btn_1">
                             <span className="relative z-10 text-[13px] py-4">Products</span>
                         </button>
