f23363ef00c4a780170dcec18dab53fa34e00a81
Sai Manikanta <mani333007@gmail.com>
2024-03-13 14:42:51 +0530

    uncommented json data

--- components/mylapay-products-apis/ProductsTabs.jsx
+++ components/mylapay-products-apis/ProductsTabs.jsx
@@ -4,42 +4,42 @@ import { GoGraph } from "react-icons/go"
 import { IoMdSwitch } from "react-icons/io"
 import { RiSecurePaymentLine } from "react-icons/ri";
 
-// const requestJSON = {
-//     "menu": {
-//         "id": "file",
-//         "value": "File",
-//         "popup": {
-//             "menuitem": [
-//                 { "value": "New", "onclick": "CreateNewDoc()" },
-//                 { "value": "Open", "onclick": "OpenDoc()" },
-//                 { "value": "Close", "onclick": "CloseDoc()" }
-//             ]
-//         }
-//     }
-// }
+const requestJSON = {
+    "menu": {
+        "id": "file",
+        "value": "File",
+        "popup": {
+            "menuitem": [
+                { "value": "New", "onclick": "CreateNewDoc()" },
+                { "value": "Open", "onclick": "OpenDoc()" },
+                { "value": "Close", "onclick": "CloseDoc()" }
+            ]
+        }
+    }
+}
 
-// const responseJSON = {
-//     "glossary": {
-//         "title": "example glossary",
-//         "GlossDiv": {
-//             "title": "S",
-//             "GlossList": {
-//                 "GlossEntry": {
-//                     "ID": "SGML",
-//                     "SortAs": "SGML",
-//                     "GlossTerm": "Standard Generalized Markup Language",
-//                     "Acronym": "SGML",
-//                     "Abbrev": "ISO 8879:1986",
-//                     "GlossDef": {
-//                         "para": "A meta-markup language, used to create markup languages such as DocBook.",
-//                         "GlossSeeAlso": ["GML", "XML"]
-//                     },
-//                     "GlossSee": "markup"
-//                 }
-//             }
-//         }
-//     }
-// }
+const responseJSON = {
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
 
 function ProductsTabs() {
     return (
