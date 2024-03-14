8a424fcc85ac40ece38d3fbca35c25db2a563188
Sai Manikanta <mani333007@gmail.com>
2024-03-14 16:40:03 +0530

    Authorization page

--- pages/authorization.js
+++ pages/authorization.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 
 import Header from '../components/layout/Header'
 import Benefits from '../components/authorization/Benefits'
+import Support from '../components/authorization/Support'
 // import Banner from '../components/authorization/Banner'
 // import ProductsTabs from '../components/authorization/ProductsTabs'
 // import Steps from '../components/authorization/Steps'
@@ -20,25 +21,21 @@ function Authorization() {
         <div className={`${inter.className}`}>
             <Header />
 
-            <div className='p-4 bg-bggray'>
+            <div className='p-4 bg-bggray pb-0'>
                 <div className='bg-[#fff] '>
                     <div className="wrapper xxl:mx-[-30px]">
-                        <div className="max-w-4xl mx-auto py-20 px-4 sm:px-0">
-                            <div className="mb-10 md:mb-14">
-                                <h2 className="text-xl font-bold text-bluedark sm:text-3xl md:text-4xl text-center mb-8 px-2">
+                        <div className="mx-auto py-20 px-4 sm:px-0">
+                            <div>
+                                <h2 className="text-xl font-bold text-bluedark sm:text-3xl md:text-4xl text-center mb-8">
                                     What is Mylapay Switch?
                                 </h2>
 
-                                <p className="mb-6 text-lg text-para max-w-4xl mx-auto px-2">
+                                <p className="mb-6 text-lg text-para mx-auto">
                                     Mylapay Switch empowers global aggregators, gateways, and banks with a robust and secure payment authorization solution. It seamlessly facilitates both online and offline transactions, ensuring compatibility with major card networks like Visa, Mastercard, and Rupay.
                                 </p>
-
-
-                                <p className="mb-6 text-lg text-para max-w-4xl mx-auto px-2">
+                                <p className="text-lg text-para mx-auto">
                                     Embedded with comprehensive risk management tools, Mylapay Switch safeguards every transaction against fraud, while its intelligent algorithms optimize approval rates. This synergy of security and efficiency makes it the ideal choice for modernizing payment infrastructure.
                                 </p>
-
-
                             </div>
                         </div>
                     </div>
@@ -49,6 +46,8 @@ function Authorization() {
 
 
             <Benefits />
+
+            <Support />
 
             {/* <Banner /> */}
 
